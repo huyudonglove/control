15d0d03f6f7660c9e5bd085b9f82ba17646dea96
huyudong <379145942@qq.com>
2020-07-15 16:40:27 +0800

    'login'

--- middlecontrol/src/http/request.js
+++ middlecontrol/src/http/request.js
@@ -2,7 +2,7 @@ import http from '../http'
 import {loadRequest} from '../util' //loading遮罩层
 import { Message,Loading } from 'element-ui';
 const login=(msg)=>{
-    return http.post('/api/recorded/access/login', msg)
+    return http.post('/api/remote/control/access/login', msg)
 }
 
 /* 获取验证码h */
